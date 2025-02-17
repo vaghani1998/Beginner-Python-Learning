6265f2ab4607dda3eaed5bf58933c41cc68a5e7b
DHRUMIL VAGHANI <dhrumilvaghani006@gmail.com>
2025-02-17 21:19:23 +0100

    Initial commit

---
+++ Built_In_Fun.py
@@ -0,0 +1,29 @@
+# A function represent as procedure.
+# This function can accept input (which are called arguments)
+# A function procedure type is return type, which is the 'Output' of the function.
+
+Check = len('Python is fun')
+
+print(Check)
+
+Char = str(3.14)
+
+print(Char)
+
+Int = int('45')
+
+print(Int)
+
+Float = float('56.45')
+
+print(Float)
+
+Data_type = type(45.8)
+Data_type1 = type('Hello World!')
+Data_type2 = type(True)
+
+print(Data_type)
+print(Data_type1)
+print(Data_type2)
+
+
