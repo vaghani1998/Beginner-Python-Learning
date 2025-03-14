88b2c37e77463740f2abf345b7c19f4916e2eead
DHRUMIL VAGHANI <dhrumilvaghani006@gmail.com>
2025-03-14 13:44:09 +0100

    How to Duplicate method work in dataset values

--- Module_Datatype_Origin.py
+++ Module_Datatype_Origin.py
@@ -36,7 +36,7 @@ print(emp_lat)
 emp_lat.info()
 
 # Employees who are either senior management OR started before january 1st, 1990
-
+print('Employees who are either senior management OR started before january 1st, 1990')
 emp_lat = pd.read_csv('Csv_file/employees.csv', parse_dates=['Start Date'])
 
 senior_Manager = emp_lat['Senior Management']
@@ -45,7 +45,7 @@ emp_lat = emp_lat[senior_Manager | start_date]
 print(emp_lat)
 
 # First Name is Robert who works in client Services OR start date after 01/06/2016
-
+print('First Name is Robert who works in client Services OR start date after 01/06/2016')
 emp_work = pd.read_csv('Csv_file/employees.csv', parse_dates=['Start Date']).dropna(how='all')
 
 client_Name = emp_work['First Name'] == 'Robert'
@@ -54,6 +54,26 @@ is_date = emp_work['Start Date'] > '2016-06-01'
 emp_work = emp_work[client_Name & is_client | is_date]
 print(emp_work)
 
+# Between Method, how-to work Dataset
+# Employee Salary Between 45000 to 55000
+
+emp_work = emp_work[emp_work['Salary'].between(130590, 14000)]
+print(emp_work)
+emp_work.info()
+
+# How to duplicate Values check and find the index
+
+emp_duplicate = pd.read_csv('Csv_file/employees.csv')
+
+col1 = emp_duplicate[emp_duplicate['Team'].duplicated()]
+duplicate = emp_duplicate[emp_duplicate['First Name'].duplicated(keep='first')]
+
+print(col1)
+print(duplicate)
+emp_duplicate.info()
+
+opposite = emp_duplicate[~emp_duplicate['First Name'].duplicated(keep=False)]
+print(opposite)
 
 
 
