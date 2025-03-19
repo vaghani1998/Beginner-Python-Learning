6e723e8ba86d07187c77e32cb2dd24827266a58f
DHRUMIL VAGHANI <dhrumilvaghani006@gmail.com>
2025-03-19 12:36:08 +0100

    How to Multi index use

--- Overwrite_method.py
+++ Overwrite_method.py
@@ -48,3 +48,48 @@ bond[['First', 'Last']] = bond['First Name'].str.split(",", expand=True)
 
 print(bond)
 
+# How can set indexing
+
+bond = pd.read_csv('Csv_file/employees.csv')
+
+bond = bond.set_index('Team')
+
+both = bond.set_index(keys=["First Name", "Start Date"]).head(15).sort_index()
+
+print(both)
+
+# How to get_level_values method extracts an index with the values from one level in the multiple.
+
+multi = pd.read_csv('Csv_file/employees.csv', parse_dates=['Start Date'], date_format='%m-%d-%Y', index_col=['Start Date', 'First Name', 'Team']).sort_index()
+
+level = multi.index.get_level_values('Start Date')
+date = level.get_level_values(0)
+print(date)
+
+level1 = multi.index.get_level_values('Team')
+name = level1.get_level_values(0)
+print(name)
+
+print(multi)
+multi.info()
+
+# How to rename index level then multiple index level:
+
+index = multi.index.set_names(names='Date', level=0).to_frame().iloc[5:9]
+
+index1 = multi.index.set_names(names='Name', level=1)
+
+print('hallo world!', index)
+print(index1)
+
+# I order data Team and Start date wise:
+
+exe = pd.read_csv('Csv_file/employees.csv', parse_dates=['Start Date'], date_format='%m-%d-%Y')
+exe['Team'] = exe['Team'].fillna('Sales Mens')
+exe1 = exe['Start Date'].dtypes
+print(exe1)
+exe = exe.set_index(keys=['Team', 'Start Date'])
+# exe = exe.sort_index(ascending=False)
+exe = exe.sort_index(ascending=[True, False])
+exe.info()
+print(exe)
